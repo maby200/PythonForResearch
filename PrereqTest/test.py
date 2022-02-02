c6c942d7b818c93935d883f4b7ffeb5e701e0c74
maby200 <maby200@gmail.com>
2022-02-02 18:02:04 -0500

    Add files via upload

---
+++ PrereqTest/test.py
@@ -0,0 +1,12 @@
+x = 1
+while x < 5:
+    x *= 2
+print(x)
+
+x = 'String'
+y = 10
+z = 5.0
+print(x + x) # print command 1
+print(y + y) # print command 2
+print(y + z) # print command 4
+print(y + x) # print command 3
